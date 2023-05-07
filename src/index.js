7f7d6b3b961a76cc36b7143f56e0175374ba95b0
Ardavan Zareirad <ardavan2525@gmail.com>
2023-05-07 02:20:02 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ const form= document.querySelector("form")
+ const addTask= document.querySelector("#tasks")
+ 
+ form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+ const newTask=document.createElement("li")
+ newTask.innerText=e.target[0].value;
+ const removeBtn= document.createElement("button");
+ removeBtn.innerHTML="remove"
+ removeBtn.addEventListener("click", deleteTask) 
+ newTask.appendChild(removeBtn);
+ addTask.appendChild(newTask);
+ })
 });
+
+function deleteTask(e){
+  console.log("Hello")
+  e.preventDefault();
+  e.currentTarget.parentElement.remove();
+}
+
